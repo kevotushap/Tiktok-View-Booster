0a0c7a29e3017563f6ded7189e3294490ce0f459
r3ci <139361332+R3CI@users.noreply.github.com>
2024-08-24 00:17:50 +0200

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,32 @@
+import os
+try:
+    import requests
+    import tls_client
+    import string
+    from typing import Tuple
+    import pystyle
+    import time
+    import random
+    import re
+    import uuid
+    import json
+    import datetime
+    import base64
+    import urllib.parse
+    from colorama import Fore, Back, init, Style as S; init(autoreset=True)
+    from urllib.parse import urlparse, unquote
+
+except ModuleNotFoundError:
+    libs = [
+        'requests',
+        'tls-client',
+        'uuid',
+        'datetime',
+        'colorama',
+        'pystyle'
+    ]
+
+    for lib in libs:
+        os.system(f'pip install {lib}')
+
+    input('Installed all libs! Please re run!')
