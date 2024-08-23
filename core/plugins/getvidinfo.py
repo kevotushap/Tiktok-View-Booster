0872e94b042af126ca999a9446a904f5d624467d
r3ci <139361332+R3CI@users.noreply.github.com>
2024-08-24 00:56:38 +0200

    🟢🟢🟢https://discord.gg/AQC2ARgamK🟢🟢🟢
    🟢🟢🟢https://discord.gg/AQC2ARgamK🟢🟢🟢
    🟢🟢🟢https://discord.gg/AQC2ARgamK🟢🟢🟢
    🟢🟢🟢https://discord.gg/AQC2ARgamK🟢🟢🟢
    🟢🟢🟢https://discord.gg/AQC2ARgamK🟢🟢🟢
    🟢🟢🟢https://discord.gg/AQC2ARgamK🟢🟢🟢

--- core/plugins/getvidinfo.py
+++ core/plugins/getvidinfo.py
@@ -4,16 +4,23 @@ from core.plugins.log import *
 from core.plugins.sess import *
 
 class getvidinfo:
-    def get():
+    def get(retries=0):
         ses, headers = sess.get()
 
+        headers['Authority'] = 'tiktok.livecounts.io'
+        headers['Origin'] = 'https://livecounts.io'
+
         r = ses.get(
-            f'https://tiktok.livecounts.io/video/stats/{cfg().get().rsplit("/", 1)[-1]}', 
+            f'https://tiktok.livecounts.io/video/stats/{cfg().get().rsplit('/', 1)[-1]}', 
             headers=headers
         )
 
         if r.json()['success'] == True:
             return r.json()['viewCount'], r.json()['likeCount'], r.json()['commentCount'], r.json()['shareCount']
         else:
+            retries += 1
             log.error('GET VID INFO', r.text)
-            getvidinfo.get()
+            if retries > 3:
+                return '???', '???', '???', '???'
+
+            getvidinfo.get(retries)
